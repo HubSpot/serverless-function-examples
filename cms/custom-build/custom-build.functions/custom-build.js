d56f51af26d355b696457ffe15caa03114d30900
Mike Talley <mtalley@hubspot.com>
2021-01-28 16:43:55 -0500

    added params to custom-build response

--- cms/custom-build/custom-build.functions/custom-build.js
+++ cms/custom-build/custom-build.functions/custom-build.js
@@ -3,9 +3,13 @@
 const faker = require('faker');
 
 exports.main = (functionContext, sendResponse) => {
+  const { params } = functionContext;
+
   sendResponse({ body: {
     name: faker.name.findName(),
     email: faker.internet.email(),
+    // This will include any query params added to the URL to the function output
+    params,
     contactCard: faker.helpers.createCard()
   }, statusCode: 200 });
 };
