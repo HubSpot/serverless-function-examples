6c09e6213ae8284047386dff68b315a0b26753c6
Mike Talley <mtalley@hubspot.com>
2021-02-01 16:35:31 -0500

    updated other tests with describe blocks

--- cms/custom-build/custom-build.functions/custom-build.test.js
+++ cms/custom-build/custom-build.functions/custom-build.test.js
@@ -1,25 +1,27 @@
 const frisby = require('frisby');
 
-beforeAll(() => {
-  frisby.addExpectHandler('hasCorrectProperties', (response) => {
-    let json = JSON.parse(response.body);
+describe('custom-build', () => {
+  beforeAll(() => {
+    frisby.addExpectHandler('hasCorrectProperties', (response) => {
+      let json = JSON.parse(response.body);
 
-    expect(json.name).toEqual(expect.any(String));
-    expect(json.email).toEqual(expect.any(String));
-    expect(json.contactCard).toEqual(expect.any(Object));
+      expect(json.name).toEqual(expect.any(String));
+      expect(json.email).toEqual(expect.any(String));
+      expect(json.contactCard).toEqual(expect.any(Object));
+    });
   });
-});
 
-it('should return a 200', () => {
-  return frisby.get('http://localhost:5432/_hcms/api/custom-build')
-    .expect('status', 200);
-});
+  it('should return a 200', () => {
+    return frisby.get('http://localhost:5432/_hcms/api/custom-build')
+      .expect('status', 200);
+  });
 
-it('should return the proper data shape', () => {
-  return frisby.get('http://localhost:5432/_hcms/api/custom-build')
-    .expect('hasCorrectProperties');
-});
+  it('should return the proper data shape', () => {
+    return frisby.get('http://localhost:5432/_hcms/api/custom-build')
+      .expect('hasCorrectProperties');
+  });
 
-afterAll(() => {
-  frisby.removeExpectHandler('hasCorrectProperties');
+  afterAll(() => {
+    frisby.removeExpectHandler('hasCorrectProperties');
+  });
 });
